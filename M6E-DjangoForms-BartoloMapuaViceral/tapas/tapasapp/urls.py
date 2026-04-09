a464b7191249da087e8aff7a0ec69f6ec1d93f1a
kurokabe8 <bartolomarco8@gmail.com>
2026-04-09 09:17:06 +0800

    models and urls.py files edited

--- M6E-DjangoForms-BartoloMapuaViceral/tapas/tapasapp/urls.py
+++ M6E-DjangoForms-BartoloMapuaViceral/tapas/tapasapp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.better_menu, name='better_menu'),
-    path('add_menu', views.add_menu, name='add_menu'),
+    path('', views.login_view, name='login'), 
+    
+    path('signup/', views.signup_view, name='signup'), 
+    path('basic_list/<int:pk>/', views.basic_list, name='basic_list'), 
+    path('manage_account/<int:pk>/', views.manage_account, name='manage_account'), 
+    path('change_password/<int:pk>/', views.change_password, name='change_password'), 
+
+    path('better_menu/', views.better_menu, name='better_menu'), 
+    path('add_menu/', views.add_menu, name='add_menu'),
     path('view_detail/<int:pk>/', views.view_detail, name='view_detail'),
     path('delete_dish/<int:pk>/', views.delete_dish, name='delete_dish'),
     path('update_dish/<int:pk>/', views.update_dish, name='update_dish'),
